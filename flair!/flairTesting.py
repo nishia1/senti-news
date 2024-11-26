79f45d1614aba4e63616c5e08b889ea74cfbc70e
Nishi Agrawal <agrawalnishi007@gmail.com>
2024-11-25 19:59:33 -0500

    flair and half of text blob

---
+++ flair!/flairTesting.py
@@ -0,0 +1,7 @@
+from flair.models import TextClassifier
+from flair.data import Sentence
+classifier = TextClassifier.load('en-sentiment')
+sentence = Sentence('Flair is pretty neat!')
+classifier.predict(sentence)
+# print sentence with predicted labels
+print('Sentence above is: ', sentence.labels)
